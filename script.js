bda9dc48ed4447bab192378813d99874aa339281
beatriz andre <biacaram@hotmail.com>
2020-12-15 20:21:51 -0200

    toggleClock

--- script.js
+++ script.js
@@ -17,3 +17,31 @@ pauseButton.addEventListener('click', () => {
 stopButton.addEventListener('click', () => {
   toggleClock(true);
 })
+
+let isClockRunning = false;
+
+// in seconds = 25 mins
+let workSessionDuration = 1500;
+let currentTimeLeftInSession = 1500;
+
+// in seconds = 5 mins;
+let breakSessionDuration = 300;
+
+const toggleClock = (reset) => {
+  if (reset) {
+    // STOP THE TIMER
+  } else {
+    if (isClockRunning === true) {
+      // PAUSE THE TIMER
+      clearInterval(clockTimer);
+      isClockRunning = false;
+    } else {
+      // START THE TIMER
+      isClockRunning = true;
+      clockTimer = setInterval(() => {
+      // decrease time left / increase time spent
+      currentTimeLeftInSession--;
+      }, 1000)
+    }
+  }
+}
